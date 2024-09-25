bdd9f8002789668db02388fe62dc73f97770a9f4
sathvikaandekar <sathvikaandekar@gmail.com>
2024-09-25 12:22:07 +0530

    Add files via upload

---
+++ integer.dart
@@ -0,0 +1,11 @@
+void main(){
+int num1=10;
+number1 int num2=3;
+int sum=num1+num2;
+int diff=num1-num2;
+int mul=num1*num2;
+double div=num1/num2;
+print("the sum is $sum1");
+print("the mul is $mul");
+print('the div is $div");
+}
